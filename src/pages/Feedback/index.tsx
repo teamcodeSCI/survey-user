48e0926f5a3b23f4ddcb58f3be8bfbba0e33e04b
doanduc2000 <doanminhducvip2000@gmail.com>
2023-06-29 08:47:39 +0700

    update

--- src/pages/Feedback/index.tsx
+++ src/pages/Feedback/index.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './feedback.module.scss';
 import { useSelector } from 'react-redux';
 import { backgroundSelector, logoSelector, nameSelector, phoneSelector } from '@/features/brand/brandSlice';
+
+interface InfoState {
+  name: string;
+  phone: string;
+  feedback: string;
+}
 
 const Feedback = () => {
   const name = useSelector(nameSelector);
   const backgroundColor = useSelector(backgroundSelector);
   const logo = useSelector(logoSelector);
   const phone = useSelector(phoneSelector);
+  const [info, setInfo] = useState<InfoState>({ name: '', phone: '', feedback: '' });
+  const [error, setError] = useState<string>('');
+  const handleCall = () => {
+    window.location.href = `tel:${phone}`;
+  };
+  const handleInfo = (e: any) => {
+    setInfo({ ...info, [e.target.name]: e.target.value });
+  };
+  const handleSave = () => {
+    if (info.name === '' || info.phone === '' || info.feedback === '') {
+      setError('Vui lòng nhập đủ thông tin !');
+      return;
+    }
+    setError('');
+  };
   return (
     <div className={style['feedback']}>
       <div className={style['desc']}>
@@ -17,18 +38,32 @@ const Feedback = () => {
           Chúng tôi vô cùng biết ơn nếu bạn để lại nhận xét về trải nghiệm của bạn tại đây !
         </p>
         <span>
-          <i>Hotline:</i> <b style={{ color: backgroundColor }}>{phone}</b>
+          <i>Hotline:</i>
+          <b style={{ color: backgroundColor }} onClick={handleCall}>
+            {phone}
+          </b>
         </span>
       </div>
       <div className={style['box']}>
         <span style={{ color: backgroundColor }}>Góp ý phản hồi</span>
         <div className={style['form']}>
-          <input type="text" name="name" placeholder="Họ và tên" />
-          <input type="text" name="phone" placeholder="Số điện thoại" />
-          <textarea name="desc" rows={10} placeholder="Nội dung phản ánh"></textarea>
+          <input type="text" name="name" placeholder="Họ và tên" value={info.name} onChange={handleInfo} />
+          <input type="text" name="phone" placeholder="Số điện thoại" value={info.phone} onChange={handleInfo} />
+          <textarea
+            name="feedback"
+            rows={10}
+            placeholder="Nội dung phản ánh"
+            onChange={handleInfo}
+            value={info.feedback}
+          >
+            {info.feedback}
+          </textarea>
         </div>
         <div className={style['btn']}>
-          <button style={{ background: backgroundColor }}>Gửi phản ánh</button>
+          {error !== '' && <p>{error}</p>}
+          <button style={{ background: backgroundColor }} onClick={handleSave}>
+            Gửi phản ánh
+          </button>
         </div>
       </div>
     </div>
