d5140ff788d1d4e7af8aa8867001ea502cb82287
doanduc2000 <doanminhducvip2000@gmail.com>
2023-09-15 16:13:05 +0700

    update

--- src/apis/survey.ts
+++ src/apis/survey.ts
@@ -7,16 +7,16 @@ export const fetchSurvey = createAsyncThunk('survey/fetchSurvey', async (id: num
 });
 export const postSurvey = createAsyncThunk('survey/postSurvey', async (survey: any) => {
   const res = await fetch('https://dev.scigroup.com.vn/api/v1/create-survey-user-input-web', {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'no-cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
+    method: 'POST',
+
+    // mode: 'no-cors',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: TOKEN,
     },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(survey), // body data type must match "Content-Type" header
+
+    body: JSON.stringify(survey),
+    redirect: 'follow',
   });
   return await res.json();
 });
