c8d39b3748b8b319365da15d5c00061a7056a623
doanduc2000 <doanminhducvip2000@gmail.com>
2023-09-25 08:35:12 +0700

    update

--- src/apis/feedback.ts
+++ src/apis/feedback.ts
@@ -3,10 +3,22 @@ import http from './http';
 import { TOKEN } from '@/utils/const';
 import { InfoState } from '@/models/feedback';
 
-export const createFeedback = createAsyncThunk(
-  'feedback/createFeedback',
-  async (body: InfoState) =>
-    await http.get(
-      `/create-case-survey?token=${TOKEN}&name=${body.name}&phone=${body.phone}&content=${body.content}&company_id=${body.company_id}&type=${body.type}`,
-    ),
-);
+// export const createFeedback = createAsyncThunk(
+//   'feedback/createFeedback',
+//   async (body: InfoState) =>
+//     await http.get(
+//       `/create-case-survey?token=${TOKEN}&name=${body.name}&phone=${body.phone}&content=${body.content}&company_id=${body.company_id}&type=${body.type}`,
+//     ),
+// );
+export const createFeedback = createAsyncThunk('feedback/createFeedback', async (body: InfoState) => {
+  const res = await fetch('https://dev.scigroup.com.vn/api/v1/create-crm-case-survey', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: TOKEN,
+    },
+
+    body: JSON.stringify(body),
+  });
+  return await res.json();
+});
